c42ad22101545b6c2b3bdaac852ba3cd909330ff
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-27 16:26:48 +0200

    Bundle 2 Esercizio 1

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -11,37 +11,44 @@
 
 
 // ESERCIZIO 1
-for (let i = 0; i > 5; i++) {
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 
+//ad ogni iterazione il codice mi fa un log di i
 
-// ESERCIZIO 2
-function addIfEven(num) {
-    if (num % 2 = 0) {
-        return num + 5;
-    }
-    return num;
-}
+//in questo caso il bug è dovuto alla condizione del ciclo for
+//essendo i = 0 allora non sarà mai maggiore di 5
+
+//nulla da segnalare
 
 
-// ESERCIZIO 3
-function loopToFive() {
-    for (let i = 0, i < 5, i++) {
-        console.log(i);
-    }
-}
+// // ESERCIZIO 2
+// function addIfEven(num) {
+//     if (num % 2 = 0) {
+//         return num + 5;
+//     }
+//     return num;
+// }
 
 
-// ESERCIZIO 4 (suggerimento: ci sono 8 errori)
-function displayEvenNumbers() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-    let evenNumbers = [];
-    for (let i = 0; i < numbers.length - 1; i++;) {
-        if (numbers % 2 = 0); {
-            evenNumbers.push(i);
-        }
-        return evenNumbers;
-    }
-}
-displayEvenNumbers(); // [2,4,6,8]
+// // ESERCIZIO 3
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
+
+
+// // ESERCIZIO 4 (suggerimento: ci sono 8 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // [2,4,6,8]
