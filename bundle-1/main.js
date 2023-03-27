e0a5da66a16fe1d307615f28326b8df15bde8be7
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-27 16:08:48 +0200

    Esercizio 2

--- bundle-1/main.js
+++ bundle-1/main.js
@@ -11,19 +11,19 @@
 
 
 // ESERCIZIO 1
-function checkAge() {
-    const myAge = 32;
-    let message = '';
+// function checkAge() {
+//     const myAge = 32;
+//     let message = '';
 
-    if (myAge < 18) {
-        message = `Sei troppo giovane! Hai ${myAge} anni!`;
-    } else {
-        message = 'Hai più di 18 anni!';
-    }
-    return message;
-}
-checkAge();
-console.log(checkAge());
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+//     return message;
+// }
+// checkAge();
+// console.log(checkAge());
 
 //è una funzione che mi permette definire un messaggio differnte, 
 //per un utente maggiorenne o minorenne
@@ -36,12 +36,19 @@ console.log(checkAge());
 //myAge dovrebbe essere un argomento della funzione
 
 
-// // ESERCIZIO 2
-// function printColorsNumber() {
-//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-// }
-// printColorsNumber();
+// ESERCIZIO 2
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
+
+//questa funzione dice quanti colori sono presenti nella mia palette
+
+//il bug era dovuto al metodo length scritto in maniera scorretta
+
+//prbabilmente l'arrey dei colori dovrebbe essere al di fuori, e la funzione dovrebbe
+//avere poi come argomento l'arrey dei colori in modo da essere riutilizzabile
 
 
 // // ESERCIZIO 3
